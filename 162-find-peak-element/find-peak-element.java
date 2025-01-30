502b94aa60edaa09139a823f754a129e5d0ee9a6
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-01-30 12:47:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (7.38%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+         while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // We are in the decreasing part of the array.
+                // This may be the answer, but look at the left.
+                end = mid;
+            } else {
+                // We are in the increasing part of the array.
+                // So the peak must be on the right.
+                start = mid + 1;
+            }
+        }
+        return start; // Or end, both are correct.
+    }
+}
