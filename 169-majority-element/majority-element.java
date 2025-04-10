ac33f084ecde6b818cb683fa98f438ecc4c32076
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-04-10 16:37:32 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (29.42%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,24 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-
-        // Iterate through all elements using two loops
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-
-            // Compare nums[i] with the rest of the elements
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
+        return findMajority(nums,0,nums[0]);
+    }
+    public static int findMajority(int[] nums,int in,int va){
+        int c=0;
+        for(int i=in;i<nums.length;i++){
+            if(nums[i]==va)
+                c++;
+            else {
+                c--;
             }
-
-            // Check if the current element is the majority element
-            if (count > n / 2) {
-                return nums[i];
+            if(c==-1){
+                return findMajority(nums,i,nums[i]);
             }
         }
-        
-         throw new IllegalArgumentException("No majority element found.");
+        return va;
     }
 }
