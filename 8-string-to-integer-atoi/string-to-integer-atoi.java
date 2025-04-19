ee21195b89b93abde2f251810022205a420ef184
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-04-19 21:11:10 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (8.60%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String s) {
+        int res = 0, sign = 1, idx = 0;
+        int n = s.length();
+
+        // Ignore leading whitespaces
+        while (idx < n && s.charAt(idx) == ' ') {
+            idx++;
+        }
+
+        // Check for sign
+        if (idx < n && (s.charAt(idx) == '-' || s.charAt(idx) == '+')) {
+            sign = (s.charAt(idx) == '-') ? -1 : 1;
+            idx++;
+        }
+
+        // Build the number
+        while (idx < n && s.charAt(idx) >= '0' && s.charAt(idx) <= '9') {
+            // Check for overflow
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && s.charAt(idx) - '0' > 7)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            res = res * 10 + (s.charAt(idx) - '0');
+            idx++;
+        }
+
+        return res * sign;
+    }
+}
