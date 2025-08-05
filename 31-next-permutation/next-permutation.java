8e5d6d7f1c673866d80e42e58f888ca75a9e10be
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-05 16:57:53 +0530

    Time: 1 ms (13.14%) | Memory: 43.5 MB (11.16%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int inflpoint = 0;
+        int n = nums.length - 1;
+
+       
+        for (int i = n; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                inflpoint = i;
+                break;
+            }
+        }
+
+        
+        if (inflpoint == 0) {
+            Arrays.sort(nums);
+        } else {
+            int toswapIndex = inflpoint - 1;
+            int minGreater = Integer.MAX_VALUE;
+            int minIndex = -1;
+
+           
+            for (int j = inflpoint; j <= n; j++) {
+                if (nums[j] > nums[toswapIndex] && nums[j] <= minGreater) {
+                    minGreater = nums[j];
+                    minIndex = j;
+                }
+            }
+
+            
+            int temp = nums[toswapIndex];
+            nums[toswapIndex] = nums[minIndex];
+            nums[minIndex] = temp;
+
+           
+            Arrays.sort(nums, inflpoint, n + 1);
+        }
+    }
+}
