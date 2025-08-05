2bc50b1b9a346eb5c181d3b96f4e7f987044bcc6
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-05 19:46:04 +0530

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.java
@@ -0,0 +1,18 @@
+// Last updated: 8/5/2025, 7:46:04 PM
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+       int[] mergedNums = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1, 0, mergedNums, 0, nums1.length);
+        System.arraycopy(nums2, 0, mergedNums, nums1.length, nums2.length);
+        Arrays.sort(mergedNums);
+
+        int n = mergedNums.length;
+        if (n % 2 == 1) {
+            return mergedNums[n / 2];
+        } else {
+            int mid1 = n / 2 - 1;
+            int mid2 = n / 2;
+            return (mergedNums[mid1] + mergedNums[mid2]) / 2.0;
+        }
+    }
+}    
