318b6b46689c3474fd511b6fbdc134a03d788471
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-05 19:45:22 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,28 @@
+// Last updated: 8/5/2025, 7:45:22 PM
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return new int[]{nums[index], index + 1};  // duplicate, missing
+            }
+        }
+
+        return new int[]{-1, -1};  // fallback
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
