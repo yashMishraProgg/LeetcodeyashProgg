04935d6c3aa8edf81af77c010bdc2d96ba1e9d6a
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-06 19:58:49 +0530

    Create 50-Pow(x,n)

---
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -0,0 +1,22 @@
+// Last updated: 8/6/2025, 7:58:49 PM
+class Solution {
+    public double myPow(double x, int n) {
+        long power = n; 
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1;
+        while (power > 0) {
+            //if pow is odd only
+            if (power % 2 == 1) {
+                result *= x;    
+            }
+            x *= x;            // Square the base
+            power /= 2;        // Divide exponent by 2
+        }
+
+        return result;
+    }
+}
