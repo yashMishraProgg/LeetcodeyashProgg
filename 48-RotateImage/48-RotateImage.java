92de54f5c928d494d2e48f461d52d28ff789db32
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-06 19:21:47 +0530

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.java
@@ -0,0 +1,28 @@
+// Last updated: 8/6/2025, 7:21:47 PM
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
