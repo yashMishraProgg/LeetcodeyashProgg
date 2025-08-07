0ec1b1ca256ea2cd8172e01a4ffdbee8c4d6acf2
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-07 22:11:35 +0530

    File updated at 8/7/2025, 10:11:34 PM

--- 493-ReversePairs/493-ReversePairs.java
+++ 493-ReversePairs/493-ReversePairs.java
@@ -1,51 +1,56 @@
-// Last updated: 8/7/2025, 3:36:59 PM
+// Last updated: 8/7/2025, 10:11:34 PM
 class Solution {
-    public int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1);
-    }
-
-    private int mergeSort(int[] nums, int low, int high) {
-        if (low >= high) return 0;
-
-        int mid = (low + high) / 2;
-        int count = mergeSort(nums, low, mid) + mergeSort(nums, mid + 1, high);
-
-        // Count how many elements in right half form a reverse pair with elements in left
-        int j = mid + 1;
-        for (int i = low; i <= mid; i++) {
-            while (j <= high && (long) nums[i] > 2L * nums[j]) {
-                j++;
-            }
-            count += (j - (mid + 1));
+    public int divide(int arr[], int lower, int upper) {
+        int count = 0;
+        if (lower >= upper) {
+            return count;
         }
-
-        // Merge the sorted halves
-        merge(nums, low, mid, high);
+            int mid = (lower + upper) / 2;
+            count += divide(arr, lower, mid);
+            count += divide(arr, mid + 1, upper);
+            count += countpaires(arr, lower, mid, upper);
+            concquer(arr, lower, mid, upper);
         return count;
     }
 
-    private void merge(int[] nums, int low, int mid, int high) {
-        int[] temp = new int[high - low + 1];
-        int i = low, j = mid + 1, k = 0;
-
-        // Merge two sorted parts into temp array
-        while (i <= mid && j <= high) {
-            if (nums[i] <= nums[j]) {
-                temp[k++] = nums[i++];
+    public void concquer(int arr[], int lower, int mid, int upper) {
+        int merged[] = new int[upper - lower + 1];
+        int idx1 = lower;
+        int idx2 = mid + 1;
+        int x = 0;
+        while (idx1 <= mid && idx2 <= upper) {
+            if (arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
             } else {
-                temp[k++] = nums[j++];
+                merged[x++] = arr[idx2++];
             }
         }
-
-        while (i <= mid) {
-            temp[k++] = nums[i++];
+        while (idx1 <= mid) {
+            merged[x++] = arr[idx1++];
         }
-
-        while (j <= high) {
-            temp[k++] = nums[j++];
+        while (idx2 <= upper) {
+            merged[x++] = arr[idx2++];
         }
+        for (int i = 0, j = lower; i < merged.length; i++, j++) {
+            arr[j] = merged[i];
+        }
+    }
 
-        // Copy back to original array
-        System.arraycopy(temp, 0, nums, low, temp.length);
+    public int countpaires(int[] arr, int lower, int mid, int upper) {
+        int right = mid + 1;
+        int count = 0;
+        for (int i = lower; i <= mid; i++) {
+            while (right <= upper && (long)arr[i] > 2 * (long)arr[right])
+                right++;               
+            count += (right - (mid + 1));           
+        }
+        return count;
+    }
+
+    public int reversePairs(int[] nums) {
+        int upper = nums.length;
+        int count = divide(nums, 0, upper - 1);
+        System.gc();
+        return count;
     }
 }
