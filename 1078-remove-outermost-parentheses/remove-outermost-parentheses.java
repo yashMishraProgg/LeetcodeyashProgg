7754ec1b7af7c05be8af2690d3f40f2e0b82cd9b
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-09 00:45:44 +0530

    Time: 2 ms (99.17%) | Memory: 42.7 MB (22.83%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                if(count>0){//Cause we've to leave first
+                //opening '('
+                    sb.append(c);
+                }
+                count++;//Entered inside outer par
+            } else{
+                count--;//for closed ')' cause if two closed
+                //')' will occur then 1(not added) (0)not added
+                if(count>0){
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
