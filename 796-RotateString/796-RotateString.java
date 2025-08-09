6276fbcedb1801f55c2fb31e826b165fd4ca7f04
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-10 01:06:39 +0530

    Create 796-RotateString

---
+++ 796-RotateString/796-RotateString.java
@@ -0,0 +1,14 @@
+// Last updated: 8/10/2025, 1:06:39 AM
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        //Basically we've to check that its rotation s
+        //should be present in the goal or not
+        //str + str = abcdeabcde
+        //every goal will lie in this
+
+        if(s.length() != goal.length()){
+            return false;
+        }
+        return (s+s).contains(goal);
+    }
+}
