6f91e1b290894d6ec8beee6e3d725c491a6b79cc
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-12 20:14:23 +0530

    Time: 6 ms (20.52%) | Memory: 45.2 MB (14.19%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        // here we use hashmap to map symbols to value
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = map.get(s.charAt(s.length() - 1)); // we start from last char
+
+        for (int i = s.length() - 2; i >= 0; i--) { // start from second last index to compare values
+            if (map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) { 
+                // i.e in "MCMXCIV" => here i(1) < i+1(5) = 5-1 that is IV(4)
+                result = result - map.get(s.charAt(i));
+            } else {
+                result = result + map.get(s.charAt(i));
+            }
+        }
+        return result;
+    }
+}
