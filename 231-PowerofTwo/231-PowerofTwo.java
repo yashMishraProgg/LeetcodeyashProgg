f81c19455afcf105baf647bcc82ad7b47c8e8c96
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-13 17:34:30 +0530

    Create 231-PowerofTwo

---
+++ 231-PowerofTwo/231-PowerofTwo.java
@@ -0,0 +1,19 @@
+// Last updated: 8/13/2025, 5:34:30 PM
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0; //any n&(n-1) always be a power of 2
+    }
+}
+//alternative
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if (n < 1) return false;
+        
+//         while (n % 2 == 0) {
+//             n /= 2;
+//         }
+        
+//         return n == 1;
+//     }
+// }
+
