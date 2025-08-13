17af74cd3120d91e588368f75d292ca5ead11dc3
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-13 17:29:47 +0530

    Time: 8 ms (84.41%) | Memory: 44.5 MB (18.53%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,6 +1,14 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        //haha max power of 3(Power 19) = 1162261467
-        return n > 0 && 1162261467 % n == 0;
+        if(n <= 0){
+            return false;
+        }
+        while(n%3 == 0){
+            n /= 3;
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
     }
 }
