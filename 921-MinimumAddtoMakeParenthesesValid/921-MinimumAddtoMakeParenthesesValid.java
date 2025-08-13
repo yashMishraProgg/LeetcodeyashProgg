b25bf140dce610ac8e3844bc56ce7c38c7a3918e
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-13 17:21:05 +0530

    Create 921-MinimumAddtoMakeParenthesesValid

---
+++ 921-MinimumAddtoMakeParenthesesValid/921-MinimumAddtoMakeParenthesesValid.java
@@ -0,0 +1,18 @@
+// Last updated: 8/13/2025, 5:21:04 PM
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int close = 0;
+
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                open ++;
+            }else if(open>0){
+                open--;
+            }else{
+                close++;
+            }
+        }
+        return open+close;
+    }
+}
