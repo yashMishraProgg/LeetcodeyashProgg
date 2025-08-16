a80c577f8aeaa00d79519d84aa0ab3ed3b763748
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-16 20:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (15.69%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximum69Number (int num) {
+        int placevalue = 0;
+        int placeValueSix  = -1;
+
+        int temp = num;
+        while(temp > 0){
+            int remain = temp % 10;
+
+            if(remain == 6){
+                placeValueSix = placevalue;
+            }
+            temp = temp / 10;
+            placevalue++;
+        }
+        if(placeValueSix == -1){ // never encountered six
+            return num;
+        }
+        return num + (int)(3 * Math.pow(10, placeValueSix));
+    }
+}
