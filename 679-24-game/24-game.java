a332e1d80b77b566057bd9f4fae5972f9f82c05f
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-18 23:43:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (92.59%) - LeetSync

--- 679-24-game/24-game.java
+++ 679-24-game/24-game.java
@@ -1,59 +1,35 @@
 class Solution {
-    public boolean judgePoint24(int[] cards) {
-        //solving using list
-        List<Double> nums = new ArrayList<>();
-        
-        for(int c: cards){
-            nums.add((double) c);
-        }
-        return game24(nums);
-    }
-
-    private boolean game24(List<Double> cards){
-        if(cards.size() == 1){//base
-            return Math.abs(cards.get(0) - 24.0)< 1e-6;//to handle float
-        }
-
-        //logic main
-        int n = cards.size();
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i == j){
-                    continue;
+    private static final double EPS = 1e-6;
+    private boolean backtrack(double[] A, int n) {
+        if(n == 1) return Math.abs(A[0] - 24) < EPS;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                double a = A[i], b = A[j];
+                A[j] = A[n-1];
+                A[i] = a + b;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = a - b;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = b - a;
+                if(backtrack(A, n - 1)) return true;
+                A[i] = a * b;
+                if(backtrack(A, n - 1)) return true;
+                if(Math.abs(b) > EPS) {
+                    A[i] = a / b;
+                    if(backtrack(A, n - 1)) return true;
                 }
-
-                List<Double> nextArr = new ArrayList<>();
-
-                for(int k = 0;k<n;k++){
-                    if(k != i && k!=j){
-                        nextArr.add(cards.get(k));
-                    }
+                if(Math.abs(a) > EPS) {
+                    A[i] = b / a;
+                    if(backtrack(A, n - 1)) return true;
                 }
-
-                double a = cards.get(i);
-                double b = cards.get(j);
-
-                List<Double> possible = new ArrayList<>();
-                possible.add(a + b);
-                possible.add(a - b);
-                possible.add(b - a);
-                possible.add(a * b);
-                if (Math.abs(b) > 1e-6){
-                    possible.add(a / b);
-                } 
-                if (Math.abs(a) > 1e-6){
-                    possible.add(b / a);
-                } 
-
-                
-                for (double val : possible) {
-                    nextArr.add(val);                // add result
-                    if (game24(nextArr)) return true; // recursive checking
-                    nextArr.remove(nextArr.size() - 1); // backtracking 
-                }
+                A[i] = a; A[j] = b;
             }
         }
         return false;
+    }
+    public boolean judgePoint24(int[] nums) {
+        double[] A = new double[nums.length];
+        for(int i = 0; i < nums.length; i++) A[i] = nums[i];
+        return backtrack(A, A.length);
     }
 }
