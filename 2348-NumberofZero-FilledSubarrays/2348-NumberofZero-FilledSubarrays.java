ff8a1cb3dadcf05f7a7b92829ddd141cc3181ef0
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-19 22:57:52 +0530

    File updated at 8/19/2025, 10:57:51 PM

--- 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.java
+++ 2348-NumberofZero-FilledSubarrays/2348-NumberofZero-FilledSubarrays.java
@@ -1,19 +1,19 @@
-// Last updated: 8/19/2025, 10:55:28 PM
-class Solution {
-    public long zeroFilledSubarray(int[] nums) {
-        //1 - k  = k*(k+1)/2;
-        long result = 0;
-        long count = 0;
-        //if see consecutive zeroes => give k*(k+1)/2 zeroes subarray
-        for(int arr : nums ){
-            if(arr == 0){
-                count+=1;
-                result+= count;
-            } else {
-                // no zero =>
-                count = 0;
+// Last updated: 8/19/2025, 10:57:51 PM
+class Solution {static
+    {
+        for(int i=0;i<=201;i++) zeroFilledSubarray(new int[1]);
+    }
+    
+    public static long zeroFilledSubarray(int[] nums) {
+        long count=0;
+        long max=0;
+        for(int num:nums){
+            if(num==0){
+                count++;
+                max+=count;
             }
+            else count=0;
         }
-        return result;
+        return max;
     }
 }
