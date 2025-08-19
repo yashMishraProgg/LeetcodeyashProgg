fbc26c418b4421a96a5de6c99ad7133a6d605f20
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-19 22:57:55 +0530

    Time: 3 ms (94.03%) | Memory: 61.5 MB (81.82%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -1,17 +1,11 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        //1 - k  = k*(k+1)/2;
-        long result = 0;
-        long count = 0;
-        //if see consecutive zeroes => give k*(k+1)/2 zeroes subarray
-        for(int arr : nums ){
-            if(arr == 0){
-                count+=1;
-                result+= count;
-            } else {
-                // no zero =>
-                count = 0;
-            }
+        long result = 0, count = 0;//k*(k+1)/2
+        // if 0 add count +1
+        //else do nothing 
+        for (int num : nums) {
+            count = (num == 0) ? count + 1 : 0; 
+            result += count;
         }
         return result;
     }
