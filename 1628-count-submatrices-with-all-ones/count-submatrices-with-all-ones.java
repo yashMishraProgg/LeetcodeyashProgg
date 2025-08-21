7c0ba51b56704b6b0f386f4da2a8f5845b082236
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-21 23:51:42 +0530

    Time: 16 ms (29.11%) | Memory: 45.3 MB (71.58%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -0,0 +1,35 @@
+class Solution {
+    private int oneDArrayCount(int[] vec) {
+        int count = 0;
+        int consecutive = 0;
+        for (int v : vec) {
+            if (v == 1) {
+                consecutive++;
+                count += consecutive;
+            } else {
+                consecutive = 0;
+            }
+        }
+        return count;
+    }
+
+    public int numSubmat(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int result = 0;
+
+        for (int startRow = 0; startRow < m; startRow++) {
+            int[] vec = new int[n];
+            Arrays.fill(vec, 1);
+
+            for (int endRow = startRow; endRow < m; endRow++) {
+                for (int col = 0; col < n; col++) {
+                    vec[col] = vec[col] & mat[endRow][col];
+                }
+                result += oneDArrayCount(vec);
+            }
+        }
+
+        return result;
+    }
+}
