eb0779479cee9c67b491a21689dc65523ff3fd2b
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-23 20:32:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (61.78%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //here we use fast and slow pointer method
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!= null && fast.next!= null){
+            fast = fast.next.next;  //2by2forward
+            slow = slow.next;       // 1by1forward
+
+            if(fast == slow){
+                return true;        //they've met (cycle confirmed)
+            }
+        }
+
+        return false;
+    }
+}
