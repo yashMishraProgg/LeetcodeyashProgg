608a48d0b80f8db5cce3a4a083ef90360d602869
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-08-26 00:33:23 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,32 @@
+// Last updated: 8/26/2025, 12:33:23 AM
+class Solution {
+
+     private int getSquare(int n){
+        int ans = 0;
+        
+        while(n>0){
+            int rem = n%10;
+            ans +=  rem*rem;
+            n/=10;
+        }
+        return ans;
+    }
+    public boolean isHappy(int n) {
+        
+    //this is fast and slow pointer method ques in disguise
+    int slow = n;
+    int fast = n;
+
+    do{
+        slow = getSquare(slow); //*1
+        fast =  getSquare(getSquare(fast)); //*2
+    }while(fast!=slow);
+
+    if(slow == 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+}
