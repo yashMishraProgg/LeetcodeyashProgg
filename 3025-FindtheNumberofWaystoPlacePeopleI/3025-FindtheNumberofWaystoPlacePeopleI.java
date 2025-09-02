8ade21b1747d08270f99690da8e274e9d9cdd33f
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-02 23:01:21 +0530

    Create 3025-FindtheNumberofWaystoPlacePeopleI

---
+++ 3025-FindtheNumberofWaystoPlacePeopleI/3025-FindtheNumberofWaystoPlacePeopleI.java
@@ -0,0 +1,43 @@
+// Last updated: 9/2/2025, 11:01:20 PM
+public class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            //A = Upper point
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            //B = Lower Right
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                if (x1 <= x2 && y1 >= y2) {
+                    boolean hasPointInside = false;
+
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j) continue;
+
+                        int x3 = points[k][0];
+                        int y3 = points[k][1];
+
+                        if (x3 >= x1 && x3 <= x2 && y3 <= y1 && y3 >= y2) {
+                            hasPointInside = true;
+                            break;
+                        }
+                    }
+
+                    if (!hasPointInside) {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
