bde142ce5f969b2a36d96030bbb720fad79057ac
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-03 23:28:58 +0530

    Time: 70 ms (56.52%) | Memory: 45.2 MB (76.81%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        //x-asc , y = desc
+        Arrays.sort(points, Comparator
+                .comparingInt((int[] p) -> p[0])
+                .thenComparingInt(p -> -p[1]));
+
+        int n = points.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int yi = points[i][1];
+            int maxY = Integer.MIN_VALUE;
+
+            for (int j = i + 1; j < n; j++) {
+                int yj = points[j][1];
+                // Bob must be not higher than Alice
+                if (yj <= yi && yj > maxY) {
+                    ans++;
+                    maxY = yj;
+                }
+            }
+        }
+        return ans;
+    }
+}
