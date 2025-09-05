bfc691fb4221601c0a5e1e76294613292bb28422
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-05 22:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (13.85%) - LeetSync

---
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+        
+        //basically we minimize the formula to 
+        //val = num1-t*num2 = express in sum of t powers of 2
+        //so the operations will be performed t times to get to 0
+       
+        int t = 1;
+
+        while(true){
+        
+        long val = num1 -(long) num2 * t;
+         
+        if(val < t){ // or val = 0 => impossible to calculate
+           return -1;
+        }
+
+        if(t >= Long.bitCount(val)){
+            return t;
+        }
+
+        t++; 
+        
+       }
+    }
+}
