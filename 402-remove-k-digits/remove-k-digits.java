699187e942196813aee32d342c6ce14b6cfc2566
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-06 19:46:27 +0530

    Time: 25 ms (40.07%) | Memory: 46 MB (23.76%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        //simple logic => pop element if previous element is greater than the next element k times 
+        //previous > next element => pop previous and add current
+
+        Stack<Character> st = new Stack<>();
+
+        //or char digit  = charAt(i)
+        for(char digit : num.toCharArray()){
+           while(!st.isEmpty() && k > 0 && st.peek() > digit){
+            st.pop();
+            k--;  // cause we have to pop till k=0;
+           }
+           //else push
+           st.push(digit);
+        }
+
+        //if no previous > next element still remove k from last
+          
+        while(k > 0 && !st.isEmpty()){
+            st.pop();
+            k--;  // cause we have to pop till k=0;
+        }
+        
+        //now concatenate the char/number from poping  out of the stack
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+
+        // as it is stack we have to reverse the order
+        sb.reverse();
+
+        //removing leading/starting zeroes
+
+        while(sb.length() > 0 && sb.charAt(0) == '0'){
+            sb.deleteCharAt(0);
+        }
+          
+        //convert the ans into string
+        return sb.length() > 0 ? sb.toString()  : "0";
+    }
+}
