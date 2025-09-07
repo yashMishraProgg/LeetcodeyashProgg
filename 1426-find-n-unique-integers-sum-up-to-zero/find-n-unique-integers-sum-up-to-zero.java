7b1bf7aa857bddd778be675e690c894fa9909c9f
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-07 19:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (51.42%) - LeetSync

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] sumZero(int n) {
+        //Logic is simple
+        //{1,-1, 2, -2, 0} = > sum = 0
+        //at i = add a number,  and at j add its -negative
+        //if 1 array space lefts add 0 
+        int[] result = new int[n];
+        int i = 0;
+        int j = n-1;
+        int start = 1;
+
+        while(i < j){
+           result[i] = start;
+           result[j] = -start;
+
+           i++;
+           j--;
+           start ++;
+        }
+
+        return result;
+    }
+}
