d59c49028bae56a962830b3f12fbeff25cee4edc
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-19 22:21:20 +0530

    Time: 2 ms (96.78%) | Memory: 41.9 MB (59.19%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        //simple logic => use stack
+        //if ( => push and check if ) is not equal to top => pop()
+        // if stack is empty => valid parenthesis
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch  == '(' || ch == '[' || ch == '{' ){
+                stack.push(ch); 
+            }
+            else{
+                
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if(ch == ')' && top != '(' ||
+                  
+                  ch == ']' && top != '[' ||
+
+                  ch == '}' && top != '{'
+                ){
+                    return false;
+                }
+            }
+        }
+       return stack.isEmpty();
+    }
+}
