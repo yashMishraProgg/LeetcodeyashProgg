557a2fd7ed4e1497f92ce3fac999f8991d07c6f3
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-23 23:56:40 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,28 @@
+// Last updated: 9/23/2025, 11:56:40 PM
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+
+        	char currFirst = s.charAt(start);
+        	char currLast = s.charAt(last);
+            //this method skips gaps , comma and other non-alphanumeric
+        	if (!Character.isLetterOrDigit(currFirst )) {
+        		start++;
+        	} else if(!Character.isLetterOrDigit(currLast)) {
+        		last--;
+        	} else {
+        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+        			return false;
+        		}
+        		start++;
+        		last--;
+        	}
+        }
+        return true;
+    }
+}
