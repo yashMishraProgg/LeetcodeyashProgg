bf8254839e3919676ed7ff8107ff1250e8ec0bab
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-28 23:50:43 +0530

    Create 976-LargestPerimeterTriangle

---
+++ 976-LargestPerimeterTriangle/976-LargestPerimeterTriangle.java
@@ -0,0 +1,19 @@
+// Last updated: 9/28/2025, 11:50:43 PM
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        //only logic is here that after sorting if
+        // a+b > c then all sides are valid for a triangle 
+
+    int n = nums.length;
+
+    Arrays.sort(nums);
+
+        for(int i = n-3; i>=0 ; i--){
+
+            if(nums[i] + nums[i+1] > nums[i+2]){
+                return nums[i] + nums[i+1] + nums[i+2];
+            }
+        }
+        return 0;
+    }
+}
