b66c30e5a75c6ff44aaefdf898f4ba53ee484dfd
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-09-30 15:22:37 +0530

    Create 2221-FindTriangularSumofanArray

---
+++ 2221-FindTriangularSumofanArray/2221-FindTriangularSumofanArray.java
@@ -0,0 +1,16 @@
+// Last updated: 9/30/2025, 3:22:37 PM
+class Solution {
+    public int triangularSum(int[] nums) {
+        //logic is simple
+        //first calculate the sum of first 2 nums and decrease the size , each time until n >= 1;
+
+        int n = nums.length;
+
+        for(int size = n-1; size >= 1; size--){
+            for(int i=0;i< size ; i++){
+                nums[i] = (nums[i] + nums[i+1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+}
