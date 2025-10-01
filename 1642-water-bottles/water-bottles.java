273dcc65ccb9c11dc34a7da89d2163a581917b2f
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-01 23:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (95.16%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        //intuiton is at the end
+        //numbottles(same filled = same empty)
+        //at the end you get net only 1 left  => (numBottles -1)
+        // Each extra drink costs (numExchange - 1)
+
+        int sum = (numBottles-1)/(numExchange-1);
+
+        return sum+numBottles;
+        
+    }
+}
