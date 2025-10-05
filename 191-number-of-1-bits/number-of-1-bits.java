8791f63bf0c120d7e0c0cf59163a783bbf88e3fb
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-05 23:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (95.20%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingWeight(int n) {
+        //The logic is simple 
+        //we take a mask and perform and if it gives a set bit => the binary contains a 1.
+        int count =  0;
+        int mask  =  1;
+
+        for(int i = 0; i< 32; i++){
+            if((n & mask) != 0){
+                count++;
+            }
+            mask  = mask << 1;
+        }
+        return count;
+    }
+}
