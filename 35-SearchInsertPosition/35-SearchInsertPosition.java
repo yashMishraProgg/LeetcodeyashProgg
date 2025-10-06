b0e882116990b01a4960690244e9d77ad7c3b28c
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-06 22:31:00 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,25 @@
+// Last updated: 10/6/2025, 10:31:00 PM
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //simple binary search
+        int left = 0;
+        int right  = nums.length -1;
+
+        while(left <= right){
+            int mid = left + (right-left) / 2;
+
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] > target){
+                right = mid -1;
+            } else{
+                left = mid + 1;
+            }
+        }
+
+        //else return left because if the target  is not found then
+        //right and mid will be at the pos less than target and
+        // left will be at the position or index where the target is to be inserted so 
+        return left;
+    }
+}
