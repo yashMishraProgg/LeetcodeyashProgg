36ef7878a4392c727be2e7a8a5e59d738ce2c41a
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-13 18:35:42 +0530

    Time: 1 ms (70.39%) | Memory: 42.3 MB (28.65%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if(root == null){
+            return result;
+        }
+
+        //creating a queue to store levels
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            // we know that levelsize == queuesize
+            int levelSize = queue.size();
+
+            TreeNode rightMostNode = null;
+
+            for(int i =0;i < levelSize ;i++){
+                
+                TreeNode currentNode = queue.poll(); //remove from queue (root first)
+
+               rightMostNode = currentNode;
+
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);   //add in queue
+                }
+
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right); //add in queue 
+                }
+
+            }
+            result.add(rightMostNode.val);
+        }
+         return result;
+    }
+}
