aca9b135ba4077dbcf1def0ec20b86fa7dceb446
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-19 01:44:37 +0530

    Time: 1 ms (99.40%) | Memory: 57.4 MB (12.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,16 +1,36 @@
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//      //Kadanes algo => Either use the previous  or make new subarray   
+//      int max_ending_here = nums[0];
+
+//      int maxSofar = nums[0];
+
+//      for(int i= 1;i<nums.length;i++){
+
+//         max_ending_here = Math.max(nums[i], nums[i] + max_ending_here);
+
+//         maxSofar = Math.max(max_ending_here, maxSofar);
+//      }
+//      return maxSofar;
+//     }
+// }
 class Solution {
     public int maxSubArray(int[] nums) {
-     //Kadanes algo => Either use the previous  or make new subarray   
-     int max_ending_here = nums[0];
-
-     int maxSofar = nums[0];
-
-     for(int i= 1;i<nums.length;i++){
-
-        max_ending_here = Math.max(nums[i], nums[i] + max_ending_here);
-
-        maxSofar = Math.max(max_ending_here, maxSofar);
-     }
-     return maxSofar;
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
     }
 }
