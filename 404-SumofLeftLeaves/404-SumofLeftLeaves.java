b6c5fabf73ed982188202b15c9fe3343af5d0b1c
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-26 21:38:21 +0530

    Create 404-SumofLeftLeaves

---
+++ 404-SumofLeftLeaves/404-SumofLeftLeaves.java
@@ -0,0 +1,38 @@
+// Last updated: 10/26/2025, 9:38:21 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return findLeft(root,false);  //cause initially root has no left and child
+    }
+
+   public int findLeft(TreeNode root, boolean isLeft){
+       if(root == null){
+        return 0;
+       }
+
+    //check if its left or not
+       if(root.left == null && root.right == null){
+          return isLeft ? root.val : 0; //if left is true return value;
+       }
+
+       //now call for left subtree of rooot
+       int a = findLeft(root.left, true); //cause we want left only
+       int b = findLeft(root.right, false);
+
+       return a + b;
+   }
+}
