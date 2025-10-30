c010da5a8472b78f9d7943df032cb0250e7136f8
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-10-30 22:05:21 +0530

    Time: 1 ms (99.73%) | Memory: 42.3 MB (49.42%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        //basic idea is if sum > 1 -> carry = 1
+        //usig stringbuilder for efficiency 
+        StringBuilder res  = new StringBuilder();
+
+        int i = a.length() - 1;      // iterate for string a
+        int j = b.length() - 1;      //iterate for string b
+
+        int carry = 0;   //intial carry should be zero
+
+        while(i >= 0 || j >= 0) {
+
+          int sum = carry;
+
+          if(i >= 0){
+            sum += a.charAt(i--) - '0';    //convert char to int
+          }
+
+          if(j >= 0){
+            sum += b.charAt(j--) - '0';
+          }
+
+          //now carry part
+          carry = sum > 1 ? 1 : 0;
+
+          res.append(sum%2);   //add the bit
+        }
+
+        if(carry !=  0){    //last carry added as it is
+            res.append(carry);
+        }
+        return res.reverse().toString();
+    }
+}
