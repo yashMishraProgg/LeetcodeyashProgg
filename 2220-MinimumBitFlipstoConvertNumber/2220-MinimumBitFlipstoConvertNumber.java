8e0d153f0552fc8f2f9200771b237d5e019e4039
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-11-02 20:16:38 +0530

    Create 2220-MinimumBitFlipstoConvertNumber

---
+++ 2220-MinimumBitFlipstoConvertNumber/2220-MinimumBitFlipstoConvertNumber.java
@@ -0,0 +1,22 @@
+// Last updated: 11/2/2025, 8:16:38 PM
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        //so the catch is we use xor to know how many bits are changed
+        //and count the 1s
+        //cause A ^ 0 = A(different 1) and A ^ A = 0(same 0)
+
+        int xor = start ^ goal; //ex 1101 -> 3 bits are changed
+        int count = 0;
+
+        while(xor > 0){
+            //xor & 1 => checks if the last bit is one 
+            count = count + (xor & 1);  
+
+            //remove the last bit
+            xor = xor >> 1;
+            
+        }
+
+        return count;
+    }
+}
