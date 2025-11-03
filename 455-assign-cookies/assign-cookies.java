dcf540d92a0358b66cc54c7d343b934b7fa48d8d
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-11-03 23:51:51 +0530

    Time: 14 ms (5.13%) | Memory: 51.3 MB (5.92%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        //so assign cookie to the least greedy child first
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i= 0; //for children
+        int j= 0; // for cookie
+
+        while(i < g.length && j < s.length){
+            //if cookie size >= gree of children give the cookie
+            if(s[j] >= g[i]){
+                i++; //give to the children
+            }
+            j++; //
+        }
+        return i;
+    }
+}
