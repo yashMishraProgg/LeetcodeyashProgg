e269bff3dd256acc164c00bdccc76bbf83e8733a
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-11-12 23:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (12.33%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -23,35 +23,92 @@
  *     }
  * }
  */
+// class Solution {
+//     public TreeNode sortedListToBST(ListNode head) {
+        
+//         //the main logic is its nothing but a question like sorted array to BST
+//         //convert ll to array - then to bst;
+
+//         List<Integer> list =  new ArrayList<>();
+
+//         while(head != null){
+//             list.add(head.val);    //add val in arr
+//             head = head.next;
+//         }
+
+//         return SortedArrayToBST(list, 0, list.size() -1);
+//     }
+
+//     public TreeNode SortedArrayToBST(List<Integer>list, int left, int right){
+//         if(left > right){
+//             return null;
+//         }    
+
+//         //calculate mid
+//         int mid = left + (right - left)/2;
+
+//         TreeNode root = new TreeNode(list.get(mid));
+
+//         root.left = SortedArrayToBST(list,left,mid - 1);
+//         root.right = SortedArrayToBST(list,mid+1,right);
+    
+//         return root;
+//     }
+// }
+
+
+
+//can also be done using left and slow pointer
+//using slow and fast pointer we cam calculate the mid of linked list
+//then using recursion we can build the bst
+
 class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         
-        //the main logic is its nothing but a question like sorted array to BST
-        //convert ll to array - then to bst;
-
-        List<Integer> list =  new ArrayList<>();
-
-        while(head != null){
-            list.add(head.val);    //add val in arr
-            head = head.next;
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return new TreeNode(head.val);
         }
 
-        return SortedArrayToBST(list, 0, list.size() -1);
-    }
 
-    public TreeNode SortedArrayToBST(List<Integer>list, int left, int right){
-        if(left > right){
-            return null;
-        }    
+        //calculate the middle of linked list using the fast and slow pointer
+        ListNode middle = getmiddle(head);
 
-        //calculate mid
-        int mid = left + (right - left)/2;
+        //we got the root so it will be the root of the linked list
 
-        TreeNode root = new TreeNode(list.get(mid));
+        TreeNode root = new TreeNode(middle.val);
 
-        root.left = SortedArrayToBST(list,left,mid - 1);
-        root.right = SortedArrayToBST(list,mid+1,right);
-    
+        //now the right and left subtree
+        root.right = sortedListToBST(middle.next);
+
+        middle.next =  null;
+        
+        root.left =sortedListToBST(head);
         return root;
     }
+
+    //calculate middle
+
+    public ListNode getmiddle(ListNode head){
+        ListNode fast = head;
+
+        ListNode slow = head;
+
+        ListNode prev = null;
+
+        while(fast != null  && fast.next != null){
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        if(prev != null) prev.next = null;
+         
+         return slow;
+    }
 }
+
+
+
