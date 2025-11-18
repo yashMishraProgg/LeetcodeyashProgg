3b982d31c7c56b3e96b352c39971c77bcc2d5ea7
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-11-18 22:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (19.07%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        //so approach is simple
+        // 0 -- is a one bit character 
+        // 1 -- it can either be a 11 or 10(2bit)
+        //logic is , if we land index on last - true
+        // if we skip the last - false
+      
+       
+
+        int i= 0;
+
+        int n = bits.length;
+
+        while( i < n - 1){
+             //if 1 bit - index += 2
+            if(bits[i] == 1){
+                i += 2;
+                  //cause if 0 bit , index++
+            }else{
+                i += 1;
+            }
+        }
+        
+        //if  the index end at last bit
+       return i == n - 1;
+
+    }
+}
