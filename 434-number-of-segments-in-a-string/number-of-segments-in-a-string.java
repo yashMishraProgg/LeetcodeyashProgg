b6f2a786694ad4df8610ad6a84ca3a75cda0f5d4
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-11-22 23:31:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (19.14%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        boolean inSegment = false;
+
+        for (char c : s.toCharArray()) {
+
+            // If current character is NOT space
+            // and we were NOT already inside a segment
+            if (c != ' ' && !inSegment) {
+                count++;        // New segment starts
+                inSegment = true;
+            }
+            // If current character IS space → segment ended
+            else if (c == ' ') {
+                inSegment = false;
+            }
+        }
+        return count;
+    }
+}
