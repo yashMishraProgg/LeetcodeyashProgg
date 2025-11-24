259f666a2482da1714320353bffcb7d6afdc3154
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-11-24 23:43:45 +0530

    Time: 3 ms (99.20%) | Memory: 47.5 MB (32.87%) - LeetSync

---
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> result = new ArrayList<>();
+        int current = 0;
+
+        for (int bit : nums) {
+            current = (current * 2 + bit) % 5; 
+            result.add(current == 0);
+        }
+
+        return result;
+    }
+}
