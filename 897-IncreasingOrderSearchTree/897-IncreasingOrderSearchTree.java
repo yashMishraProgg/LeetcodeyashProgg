8a6b32b1f1f86662d205049a56d565570f532cbc
Yash Mishra <152404057+yashMishraProgg@users.noreply.github.com>
2025-12-17 21:38:39 +0530

    File updated at 12/17/2025, 9:38:38 PM

--- 897-IncreasingOrderSearchTree/897-IncreasingOrderSearchTree.java
+++ 897-IncreasingOrderSearchTree/897-IncreasingOrderSearchTree.java
@@ -1,44 +1,21 @@
-// Last updated: 12/17/2025, 9:23:56 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    TreeNode prev = null;
-18    TreeNode head = null;
-19
-20    public TreeNode increasingBST(TreeNode root) {
-21        
-22      
-23        if (root == null){
-24            return null;
-25        }
-26
-27        increasingBST(root.left);
-28        //we dont want the left side 
-29        if(prev != null){
-30            root.left = null;
-31            prev.right = root; 
-32        }
-33
-34        //take the head as root
-35        if(head == null){
-36            head = root;
-37        }
-38        prev = root;
-39
-40        increasingBST(root.right);
-41        return head;
-42    }
-43}
+// Last updated: 12/17/2025, 9:38:38 PM
+1class Solution {
+2    public TreeNode increasingBST(TreeNode root) {
+3        TreeNode ans = new TreeNode(0);
+4        TreeNode cur = ans;
+5        inorder(root, cur);
+6        return ans.right;
+7    }
+8
+9    private TreeNode inorder(TreeNode node, TreeNode cur) {
+10        if (node == null) return cur;
+11        
+12        cur = inorder(node.left, cur);
+13        
+14        node.left = null;
+15        cur.right = node;
+16        cur = node;
+17        
+18        return inorder(node.right, cur);
+19    }
+20}
